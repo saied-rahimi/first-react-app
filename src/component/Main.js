288315eb7626fd4891093ec23ba1589c4e11073f
Saied Rahimi <saied.r.2016@gmail.com>
2024-04-18 01:51:46 +0430

    first commit

---
+++ src/component/Main.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./main.css";
+
+
+export default function Main (){
+    return(
+        <main className="Main">
+            <h2>Nilofar Abi Online and On-site shop</h2>
+            <ul>
+                <li>
+                    shose
+                </li>
+                <li>
+                    dresses
+                </li>
+                <li>
+                    feation
+                </li>
+                <li>
+                    bags
+                </li>
+                <li>and more...</li>
+            </ul>
+        </main>
+    )
+}
---
+++ src/component/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../assets/images/logo.png"; 
+
+ export default function NaveBar(){
+return(
+    <nav>
+     <img className="logo" src={logo} alt="Logo" />
+     <h3>Niloofar ABI</h3>
+     <h4>Ahmad</h4>
+    </nav>
+  
+)
+}
